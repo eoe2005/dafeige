d1f0ae055fb5aa754610cfcb82ff3beb73ff76ff
飞哥 <genghongfei@suule.com>
2016-01-12 14:27:13 +0800

    针对配置文件增加了工厂模式

--- server/php/g/conf/GConf.php
+++ server/php/g/conf/GConf.php
@@ -1,30 +1,41 @@
 <?php
+
 namespace g\conf;
+
 /**
  * @author eoe2005@qq.com
  */
 class GConf {
+
+    protected static $_selfs = [];
+
     /**
      * 
      * @param type $dns
      * @return GConfInteface
      */
-    static function ins($dns){
+    static function ins($dns) {
         $attr = explode('://', $dns);
-        switch (strtolower($attr[0])){
-            case 'ini':
-                return new GConfIni($attr[1]);
-                break;
-            case 'xml':
-                return new GConfXml($attr[1]);
-                break;
-            case 'php':
-                return new GConfPhp($attr[1]);
-                break;
-            case 'json':
-                return new GConfJson($attr[1]);
-                break;
+        $key = md5($dns);
+        if (isset(self::$_selfs[$key]) === false) {
+            switch (strtolower($attr[0])) {
+                case 'ini':
+                    self::$_selfs[$key] = new GConfIni($attr[1]);
+                    break;
+                case 'xml':
+                    self::$_selfs[$key] = new GConfXml($attr[1]);
+                    break;
+                case 'php':
+                    self::$_selfs[$key] = new GConfPhp($attr[1]);
+                    break;
+                case 'json':
+                    self::$_selfs[$key] = new GConfJson($attr[1]);
+                    break;
+                default :
+                    throw new \g\exception\GException('参数错误，配置文件不存在');
+            }
         }
-        throw  new \g\exception\GException('参数错误，配置文件不存在');
+        return self::$_selfs[$key];
     }
+
 }
