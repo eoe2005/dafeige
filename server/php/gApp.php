328c38cca6f55fa5809883e3979a0738fad2cace
飞哥 <genghongfei@suule.com>
2016-01-14 13:55:30 +0800

    修改了自动加载的功能，运行程序加载加载的路径

--- server/php/gApp.php
+++ server/php/gApp.php
@@ -11,17 +11,56 @@ define("G_DIR", realpath(dirname(__FILE__)) . DS);
 /**
  * 自动加载的类
  * */
-function gautoloadclass($cls) {
-    if ($cls == 'GLog') {
-        include G_DIR.'g'.DS.'log'.DS.'GLog.php';
-    } else {
-        $cls = preg_replace("/\\\/", DS, $cls);
-        $file = G_DIR . $cls . '.php';
-        if (file_exists($file)) {
-            include $file;
+class GAutoLoadClass {
+
+    protected static $_AUTOLOADPATH = [];
+
+    /**
+     * 添加自动加载类的路径
+     * @param type $paths
+     */
+    public static function addPaths($paths) {
+        if (is_array($paths)) {
+            foreach ($paths AS $path) {
+                $len = strlen($path);
+                if (substr($path, $len - 2) != '/') {
+                    $path .= DS;
+                }
+                if (isset(self::$_AUTOLOADPATH[$path]) === false) {
+                    self::$_AUTOLOADPATH[$path] = $path;
+                }
+            }
+        } else {
+            $len = strlen($paths);
+            if (substr($paths, $len - 2) != '/') {
+                $paths .= DS;
+            }
+            if (isset(self::$_AUTOLOADPATH[$paths]) === false) {
+                self::$_AUTOLOADPATH[$paths] = $paths;
+            }
         }
     }
+
+    public static function loadClass($cls) {
+        if ($cls == 'GLog') {
+            include G_DIR . 'g' . DS . 'log' . DS . 'GLog.php';
+        } else {
+            $cls = preg_replace("/\\\/", DS, $cls);
+            $file = G_DIR . $cls . '.php';
+            if (file_exists($file)) {
+                include $file;
+            }else{
+                foreach (self::$_AUTOLOADPATH AS $path){
+                    $file = $path.$cls;
+                    if(file_exists($file)){
+                        return include $file;
+                    }
+                }
+            }            
+        }
+    }
+
 }
 
-spl_autoload_register('gautoloadclass');
+spl_autoload_register('GAutoLoadClass::loadClass');
 
